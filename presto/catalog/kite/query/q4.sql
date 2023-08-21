760e4ba2dea43cd9ebcfc198831f40619bcb3cd0
Kat <southpolekat@yahoo.com.hk>
2023-08-21 20:31:55 +0800

    add q3 q4

---
+++ presto/catalog/kite/query/q4.sql
@@ -0,0 +1,25 @@
+USE kite.default;
+
+DROP VIEW IF EXISTS q4;
+
+CREATE VIEW q4 as 
+select
+	o_orderpriority,
+	count(*) as order_count
+from
+	orders
+where
+	o_orderdate >= date '1997-07-01'
+	and o_orderdate < date '1997-07-01' + interval '3' month
+	and exists (
+		select
+			*
+		from
+			lineitem
+		where
+			l_orderkey = o_orderkey
+			and l_commitdate < l_receiptdate
+	)
+group by
+	o_orderpriority
+;
