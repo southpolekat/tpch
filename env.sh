fa08e021e30bc25777496dcdc971664d12fa3d96
Kat <southpolekat@yahoo.com.hk>
2023-09-21 08:01:03 +0000

    default variables

--- env.sh
+++ env.sh
@@ -2,15 +2,24 @@
 
 set -e
 
+export OUTDIR=/tmp/tpch
+
+############################# KITE
+# List of kite's host:port. e.g. kite1:7878,kite2:7878
+export KITE_LOCATION=kite1:7878
+
+############################# DATA
+# Scale factor: 1 or 30
+export TPCH_SF=30 
+export TPCH_TABLES=(lineitem)
+export TPCH_QUERIES=(q1 q6)
+# Source data file format: csv or parquet 
+export DATA_FORMAT=parquet
+
+############################# PRESTO
 export PRESTO_HOME=~/presto
 export PRESTO_CLI=~/presto-cli
 export PRESTO_DATA=/mnt/disk0/presto/data
-
 # number of presto workers NOT including coordinator (p0)	
-export NUM_WORKER=8
+export PRESTO_WORKER=8
 
-# List of kite host:port. e.g. kite1:7878,kite2:7878
-export KITE_LOC=kite1:7878
-
-# tpch scale factor 1 or 30
-export TPCH_SF=30
