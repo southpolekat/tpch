1ab25d336ad2b430777119526a71a9c1afd83ab6
Kat <southpolekat@yahoo.com.hk>
2023-10-03 04:07:51 +0000

    update schema variable

--- gpdb/setup_tpch_test.sh
+++ gpdb/setup_tpch_test.sh
@@ -2,7 +2,7 @@
 
 source ../env.sh
 
-opts="-c client_min_messages=warning" # for PGOPTIONS
+opts="-c client_min_messages=warning -c optimizer=0" # for PGOPTIONS
 psql="psql -v ON_ERROR_STOP=1 -q"
 
 function setup_schema_kite {
@@ -35,7 +35,7 @@ function setup_schema_kite {
 }
 
 function setup_schema_ao {
-   schema=$SCHEMA_AO
+   schema=$GP_SCHEMA_AO
    echo "############# Schema $schema"
 
    echo "### Recreate schema"
@@ -49,8 +49,8 @@ function setup_schema_ao {
       | PGOPTIONS=${opts2} $psql
 
 	echo "### Load data"
-	for t in $TABLES; do
-		PGOPTIONS=${opts2} $psql -c "insert into $t select * from $SCHEMA_KITE.$t;"
+	for t in "${TPCH_TABLES[@]}"; do
+		PGOPTIONS=${opts2} $psql -c "insert into $t select * from $GP_SCHEMA_KITE.$t;"
 	done
 
    echo "### Analyze tables"
@@ -66,7 +66,7 @@ function setup_schema_ao {
 }
 
 function setup_schema_mixed {
-   schema=$SCHEMA_MIXED
+   schema=$GP_SCHEMA_MIXED
    echo "############# Schema $schema"
 
    echo "### Recreate schema"
@@ -78,18 +78,18 @@ function setup_schema_mixed {
 	echo "### Create views"
 	for i in {1..22}; do
    	cat query/q${i}.sql \
-			| sed -e "s/NATION/${SCHEMA_AO}.nation/" \
-			| sed -e "s/REGION/${SCHEMA_AO}.region/" \
-			| sed -e "s/PART/${SCHEMA_AO}.part/" \
-			| sed -e "s/SUPPLIER/${SCHEMA_AO}.supplier/" \
-			| sed -e "s/PARTSUPP/${SCHEMA_AO}.partsupp/" \
-			| sed -e "s/CUSTOMER/${SCHEMA_AO}.customer/" \
-			| sed -e "s/ORDERS/${SCHEMA_AO}.orders/" \
-			| sed -e "s/LINEITEM/${SCHEMA_KITE}.lineitem/" \
+			| sed -e "s/NATION/${GP_SCHEMA_AO}.nation/" \
+			| sed -e "s/REGION/${GP_SCHEMA_AO}.region/" \
+			| sed -e "s/PART/${GP_SCHEMA_AO}.part/" \
+			| sed -e "s/SUPPLIER/${GP_SCHEMA_AO}.supplier/" \
+			| sed -e "s/PARTSUPP/${GP_SCHEMA_AO}.partsupp/" \
+			| sed -e "s/CUSTOMER/${GP_SCHEMA_AO}.customer/" \
+			| sed -e "s/ORDERS/${GP_SCHEMA_AO}.orders/" \
+			| sed -e "s/LINEITEM/${GP_SCHEMA_KITE}.lineitem/" \
 			| PGOPTIONS=${opts2} $psql
 	done
 }
 
 setup_schema_kite
-#setup_schema_ao
-#setup_schema_mixed
+setup_schema_ao
+setup_schema_mixed
